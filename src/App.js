cef455e2371e77df380d5f8dafa3782eaa232edf
Moshe Weiner <moshesweiner@gmail.com>
2022-05-08 10:16:50 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
